65b92289021a54eb60967e63d06870034aaf4174
Adam Haber <adamhaber@gmail.com>
2019-11-15 08:33:26 +0200

    changed readthedocs theme

--- docs/conf.py
+++ docs/conf.py
@@ -85,7 +85,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "alabaster"
+html_theme = "default"
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
@@ -149,7 +149,7 @@ texinfo_documents = [
         u"stan2tfp Documentation",
         author,
         "stan2tfp",
-        "One line description of project.",
+        "A python wrapper around the stan2tfp compiler.",
         "Miscellaneous",
     ),
 ]
